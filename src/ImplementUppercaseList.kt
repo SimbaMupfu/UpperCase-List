93fa89c5eb27ab85abc2b09f66c3487d7ccd7c47
simba_m <scmupfururirwa@gmail.com>
2021-04-07 11:25:08 +0200

    update array values

--- src/ImplementUppercaseList.kt
+++ src/ImplementUppercaseList.kt
@@ -1,6 +1,9 @@
-fun main(args: Array<String>){
-        val names = UpperCasedList()
-        names.add("Simba")
-        names.add("Clever")
-        println("Names in upper case: $names")
-    }
+fun main(args: Array<String>) {
+    val names = UpperCasedList()
+    names.add("Simba")
+    names.add("Clever")
+    println("Names in upper case: $names")
+    names[0] = "Sims"
+    names[1] = "Hustles"
+    println("Updated names in upper case: $names")
+}
